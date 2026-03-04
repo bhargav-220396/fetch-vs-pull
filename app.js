9e3a567cf8b2627c6e3b23630eddaae470dea5a5
iambhargav-2007 <prbhargav2007@gmail.com>
2026-03-04 15:39:33 +0530

    ---
    Syntax:
    git commit
    
    Purpose:
    Creates a commit and opens an editor to write the commit message.
    
    Example:
    git commit
    
    Screenshot:
    ![git commit](screenshots/git-commit.png)
    
    ---
    
    Syntax:
    git commit -m "message"
    
    Purpose:
    Creates a commit with a message directly from the command line.
    
    Example:
    git commit -m "Added new feature"
    
    Screenshot:
    ![git commit m](screenshots/git-commit-m.png)
    
    ---
    
    Syntax:
    git commit --amend
    
    Purpose:
    Modifies the most recent commit.
    
    Example:
    git commit --amend
    
    Screenshot:
    ![git commit amend](screenshots/git-commit-amend.png)
    
    ---
    
    Syntax:
    git commit --amend --no-edit
    
    Purpose:
    Amends the last commit without changing the commit message.
    
    Example:
    git commit --amend --no-edit
    
    Screenshot:
    ![git commit no edit](screenshots/git-commit-no-edit.png)
    
    ---

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 console.log("This is the old version of code")
 console.log("This is the first new version")
 console.log("This is the second feature");
+console.log("For testing git_industry_commands");
+console.log("For git_industry_commands 2");
+console.log("For git_industry_commands 3")
+console.log("Commit command test");
