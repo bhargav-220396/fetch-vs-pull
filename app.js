14409b2d15e557fbc8eaa9784424830bd80738ce
P.Bhargav <n220396@rguktn.ac.in>
2026-03-04 09:47:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-console.log("This is new version of the code");
-console.log("This is the new feature");
-console.log("This is the second feature");
-console.log("New one");
-console.log("feature");
+console.log("This is the old version of code")
